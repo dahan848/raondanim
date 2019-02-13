00cefcbec23cb2a258e8158b5dd5d985366a5543
moonhyunglee <dlansgud5611@gmail.com>
2019-02-13 15:16:16 +0900

    병합 Test
    Controller 병합test

--- raondanim_a/src/motel/controller/motelController.java
+++ raondanim_a/src/motel/controller/motelController.java
@@ -1,11 +1,15 @@
 package motel.controller;
 
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 //숙박 관련 컨트롤러
 
 
 public class motelController {
 
-	
+	@RequestMapping("/search")
+	public String aaaaaaaaaaaa() {
+		
+		return null;
+	}
 }
